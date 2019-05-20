ea1ba6ecc6d1aaf3b153bd6dcf165e061793f5ef
Nataniel Soares Rodrigues <contatonatanielsoares@gmail.com>
2019-05-20 16:01:57 -0300

    buscando liturgia por input e melhorando regex de tag

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+let readline = require('readline-sync')
 var gerarSlide = require('./scripts/gerar-slides')
 var dividirMusicas = require('./scripts/dividir-musicas')
 var BuscarLiturgia = require('./scripts/liturgia-chamada-request')
 
+let dia = 26
+//readline.question('Digite o dia da litugia: ');
+let mes = 5
+//readline.question('Digite o mes da litugia: ');
+let ano = 2019
+//readline.question('Digite o ano da litugia: ');
+console.log(dia, mes ,ano)
 
 console.log('>Buscando liturgia do dia...');
-BuscarLiturgia(function(liturgia){
+BuscarLiturgia(dia, mes, ano,function(liturgia){
   console.log('> Liturgia buscada com sucesso!');
   console.log('> Separando estrutura das musicas...')
   let musicas = dividirMusicas();
--- scripts/liturgia-chamada-request.js
+++ scripts/liturgia-chamada-request.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
-URL = 'http://liturgiadiaria.cnbb.org.br/app/user/user/UserView.php?ano=2019&mes=5&dia=19';
 
-var BuscarLiturgia = function(response){
+
+var BuscarLiturgia = function(dia, mes, ano, response){
+    URL = 'http://liturgiadiaria.cnbb.org.br/app/user/user/UserView.php?ano='+ano+'&mes='+mes+'&dia='+dia;
+    console.log("Chamando... "+URL)
     http.get(URL, (resp) =>{
       let data = '';
 
       console.log("---Resposta do server começou");
+
       console.log(`---server status: ${resp.statusCode}`);
       console.log("---Response Headers: %j", resp.headers);
 
@@ -21,12 +24,14 @@ var BuscarLiturgia = function(response){
       resp.on('end', ()=>{
 
         let leituras = findTagValue('h3', data);
+        let salmos = findTagValue("div class='refrao_salmo'", data);
+        console.log(salmos);
         let liturgia = {
           dia: findTagValue('h2', data)[0],
           leitura1: leituras[0],
           salmo:leituras[1],
-          fraseSalmo:findTagValue("div class='refrao_salmo'", data)[0]
-          .replace('R. ','').replace('<br />', ''),
+          fraseSalmo:salmos[0]
+          .replace('Ou:', '').replace('R.','').replace(/<br \/>/g, ''),
           leitura2:leituras[2],
           evangelho:leituras[3]
         };
@@ -44,13 +49,13 @@ function findTagValue(tagName, html) {
   let p2 = /(\w*)(\s)?/
   let tagFinal = p2.exec(tagName)[1];
 
-  let strR = '<'+tagName+'(\\b[^>]*)?>(\\s+)?(.*?)</'+tagFinal+'>';
-//  console.log(strR);
+  let strR = '<'+tagName+'(\\b[^>]*)?>(\\s+)?((.*)?(\\s+)?(.+)?)</'+tagFinal+'>';
+  console.log(strR);
   let p = new RegExp(strR, "g");
   resultados = []
   let r;
   while ( (r = p.exec(html)) != null){
-
+    //console.log(r.length);
     let resultadoTratado = r ? r[3].replace(/\s\s+?/g, ' ').trim() : tagName+' NotFound';
 
     resultados.push(resultadoTratado);
