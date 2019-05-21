1cb73e17091d103d5f22b76aa0e029c99d5db1f3
Nataniel Soares Rodrigues <contatonatanielsoares@gmail.com>
2019-05-21 11:10:36 -0300

    adicionando slide do salmo e dividindo para conquistar funcoes slides

--- scripts/gerar-slides.js
+++ scripts/gerar-slides.js
@@ -26,12 +26,7 @@ var gerar = function (liturgia, musicas){
     //  process.stdout.write('>>Gerando  Slide  '+contadorSlide+' Gerado...');
       let estrofe = musica.estrofes[j];
 
-      var slide = pptx.addNewSlide();
-      slide.addText(musica.titulo, { x:'0%', y:'5%', w:'100%',
-       fontSize:20, color:'8F1010', bold:true, align: 'center' });
-
-      slide.addText(estrofe, { x:'5%', y:'15%', w:'90%', h: '85%', fontSize:calculaTamanhoFonte(estrofe.length),
-       color:'363636', align :'center'})
+      AddSlideTexto(pptx, musica.titulo, estrofe);
 
       process.stdout.write('>> Slide  '+contadorSlide+' Gerado com sucesso!');
 
@@ -39,18 +34,41 @@ var gerar = function (liturgia, musicas){
       contadorSlide ++;
     }
 
-    var slideDivisorio = pptx.addNewSlide();
-    slideDivisorio.addImage({
-      w: 10,
-      h: 6,
-      path:'./resources/santa-terezinha.jpg'
-    });
+    AddSlideDivisorio(pptx);
+
+    if(musica.titulo == 'HINOS DA PALAVRA'){ //salmo
+      AddSlideTexto(pptx, 'Salmo', liturgia.fraseSalmo);
+      AddSlideDivisorio(pptx);
+    }
+
+    if('' == ''){ //oracao eucaristica
+
+    }
 
   }
 
   let path = './slide-result/slide'
   pptx.save(path);
   console.log('>> Slides salvos em '+path);
+}
+
+function AddSlideTexto(pptx, titulo, conteudo){
+  var slide = pptx.addNewSlide();
+  slide.addText(titulo, { x:'0%', y:'5%', w:'100%',
+   fontSize:20, color:'8F1010', bold:true, align: 'center' });
+
+  slide.addText(conteudo, { x:'5%', y:'15%', w:'90%', h: '85%', fontSize:calculaTamanhoFonte(conteudo.length),
+   color:'363636', align :'center'})
+}
+
+function AddSlideDivisorio(pptx){
+  var slideDivisorio = pptx.addNewSlide();
+  slideDivisorio.addImage({
+    w: '100%',
+    h: '100%',
+    path:'./resources/santa-terezinha.jpg'
+  });
+
 }
 
 function calculaTamanhoFonte(tamanhoEstrofe){
