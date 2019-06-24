ce9998770fec5672f32b6ae25e872dec0a90dcae
unknown <contatonatanielsoares@gmail.com>
2019-06-24 13:59:37 -0300

    add .bat e selecionar musica

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var buscarArrayMusicas = require('./scripts/buscar-musicas');
 var SalvarMusicas = require('./scripts/salvar-musicas');
 
 ////console.log(buscarArrayMusicas);
+if(readline.question('Buscar Musicas por nome?') != "s"){
+  montarSlides();
+  return;
+}
+
 let nomeMusica = '';
 let arrayNomes = []
 while((nomeMusica = readline.question('Digite o nome da musica ou digite \'q\' para sair: ')) != 'q'){
@@ -15,34 +20,38 @@ while((nomeMusica = readline.question('Digite o nome da musica ou digite \'q\' p
 console.log(arrayNomes);
 
 let musicas = buscarArrayMusicas(arrayNomes)
-.then((musicas)=>{
-    //console.log(musicas);
-    SalvarMusicas(musicas).then(()=>{
-      let dia = 9;
-      dia = readline.question('Digite o dia da litugia: ');
-      let mes = 6
-      mes = readline.question('Digite o mes da litugia: ');
-      let ano = 2019
-      ano = readline.question('Digite o ano da litugia: ');
+      .then((musicas)=>{
+          //console.log(musicas);
+          SalvarMusicas(musicas).then(()=>{
+            montarSlides();
+          });
+      });
 
-      console.log(dia, mes ,ano)
+function montarSlides(){
+  let dia = 9;
+  dia = readline.question('Digite o dia da litugia: ');
+  let mes = 6
+  mes = readline.question('Digite o mes da litugia: ');
+  let ano = 2019
+  ano = readline.question('Digite o ano da litugia: ');
 
-      console.log('>Buscando liturgia do dia...');
-      BuscarLiturgia(dia, mes, ano,function(liturgia){
-        console.log('> Liturgia buscada com sucesso!');
-        console.log('>Buscando oração eucaristica...');
-        BuscarOracaoEucatistica(3, (oracao)=>{
-          console.log('> oração eucaristica buscada com sucesso!');
+  console.log(dia, mes ,ano)
 
-          console.log('> Separando estrutura das musicas...')
-          let musicas = dividirMusicas();
-          console.log('> Musicas separadas com sucesso!');
+  console.log('>Buscando liturgia do dia...');
+  BuscarLiturgia(dia, mes, ano,function(liturgia){
+    console.log('> Liturgia buscada com sucesso!');
+    console.log('>Buscando oração eucaristica...');
+    BuscarOracaoEucatistica(3, (oracao)=>{
+      console.log('> oração eucaristica buscada com sucesso!');
 
-          console.log('> Gerando Slides...');
-          gerarSlide(liturgia, oracao, musicas)
-          console.log('> Slides Gerados com sucesso!');
-        })
-      })
-    });
-});
+      console.log('> Separando estrutura das musicas...')
+      let musicas = dividirMusicas();
+      console.log('> Musicas separadas com sucesso!');
+
+      console.log('> Gerando Slides...');
+      gerarSlide(liturgia, oracao, musicas)
+      console.log('> Slides Gerados com sucesso!');
+    })
+  })
+}
 
